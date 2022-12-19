481145aa67a55bf7c61638d461bd80e32ba8e8f8
farwahaider12345 <111423158+farwahaider12345@users.noreply.github.com>
2022-12-19 12:38:29 +0400

    Add files via upload

---
+++ studentmanagement/src/main/java/com/student/serviceimpl/CourseServiceImpl.java
@@ -0,0 +1,68 @@
+package com.student.serviceimpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.student.entity.Course;
+import com.student.model.CourseDTO;
+import com.student.repository.CourseRepository;
+import com.student.service.CourseService;
+import com.student.util.CourseConverter;
+
+@Service
+public class CourseServiceImpl implements CourseService {
+
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Autowired
+	CourseConverter courseConverter;
+
+	@Override
+	public String createCourse(Course course) {
+		String message = null;
+		courseRepository.save(course);
+		if (course != null) {
+			message = "New course saved successfully!!";
+		} else {
+			message = "Course is not saved!!";
+		}
+		return message;
+	}
+
+	@Override
+	public CourseDTO updateCourse(int id, Course course) {
+		// TODO Auto-generated method stub
+
+		Course existingCourse = courseRepository.findById(id).get();
+		existingCourse.setCourseName(course.getCourseName());
+		existingCourse.setFees(course.getFees());
+		existingCourse.setDuration(course.getDuration());
+
+		courseRepository.save(existingCourse);
+		return courseConverter.convertToCourseDTO(existingCourse);
+
+	}
+
+	@Override
+	public List<CourseDTO> getAllCourse() {
+		// TODO Auto-generated method stub
+		List<Course> courses = courseRepository.findAll();
+		List<CourseDTO> cDTO = new ArrayList<>();
+		for (Course c : courses) {
+			cDTO.add(courseConverter.convertToCourseDTO(c));
+		}
+		return cDTO;
+	}
+
+	@Override
+	public void deleteAllCourse() {
+		// TODO Auto-generated method stub
+		courseRepository.deleteAll();
+
+	}
+
+}
